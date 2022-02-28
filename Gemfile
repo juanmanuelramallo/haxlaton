2d6d78f29cdf346b7cec061e1fc79a03439563ca
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-02-27 22:47:08 -0300

    Add annotate gem

--- Gemfile
+++ Gemfile
@@ -72,4 +72,5 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem "annotate"
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: matches
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Match < ApplicationRecord
   has_one_attached :recording
 end
