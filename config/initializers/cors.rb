b594464896a8740f2333b3c18736cc3c4a6fccd4
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-02-27 22:32:06 -0300

    Add http version of haxball

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://www.haxball.com"
+    origins "https://www.haxball.com", "http://www.haxball.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
