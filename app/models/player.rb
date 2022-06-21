32f5320d78c4a5e2fae85a809ac5993f0da84fe3
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-06-21 00:55:51 -0300

    Allow blank on password attr

--- app/models/player.rb
+++ app/models/player.rb
@@ -19,5 +19,5 @@ class Player < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :elo, numericality: { greater_than_or_equal_to: 0 }
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "solo letras y números" }
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "solo letras y números", allow_blank: true }
 end
