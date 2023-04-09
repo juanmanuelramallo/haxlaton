d49e1dcc5cb48dd7a98526c4df7cc78dba46029d
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2023-04-09 16:02:04 -0300

    Automatically set the room link in the server

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -2,7 +2,13 @@ module API
   class RoomsController < ApplicationController
     def update
       if @current_room.update(room_params)
-        render json: { message: "Updated successfully" }, status: :ok
+        if @current_room.haxball_room_url_previously_changed? && @current_room.haxball_room_url.present?
+          RoomNotification.with({room_id: @current_room.id}).deliver(@current_room.created_by)
+          Player.where.not(slack_user_id: [nil, ""]).each do |player|
+            RoomNotification.with({room_id: @current_room.id, channel: player.slack_user_id}).deliver(player)
+          end
+        end
+        render json: { message: "Room updated successfully with #{@current_room.haxball_room_url}" }, status: :ok
       else
         render json: { errors: @current_room.errors.full_messages }, status: :unprocessable_entity
       end
